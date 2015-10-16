8036e6dbcba4cfaa95598c26ea5a6ed7a3a4ba8b
shunjikonishi <swandouble@gmail.com>
2015-10-16 16:52:52 +0900

    Add test

--- es6/test/cipher.spec.js
+++ es6/test/cipher.spec.js
@@ -1,3 +1,5 @@
+import fs from "fs";
+import {Tokenizer} from "./tokenizer";
 import {assert} from 'chai';
 import {CaesarCipher, SimpleSubstitutionCipher} from '../target/cipher';
 
@@ -25,6 +27,11 @@ describe("Q4", () => {
     const cipher = new CaesarCipher(26);
     var ret = cipher.decrypt("hello world!");
     assert.equal(ret , "hello world!");
+  });
+
+  it("not use `for`", () => {
+    var str = fs.readFileSync("es6/src/cipher.js", "utf-8");
+    assert.notOk(new Tokenizer().split(str).some(v => v === "for"));
   });
 });
 
